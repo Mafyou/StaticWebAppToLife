72cebff6c1890a49a4e267f437b36a33f310002a
Mafyou <Sata77@gmail.com>
2025-02-21 20:50:47 +0100

    add friend website

---
+++ BlazorAppStaticWeb/IdeaStudio.Website/Models/Experience.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace IdeaStudio.Website.Models;
+
+/// <summary>
+/// Represents an individual work experience
+/// </summary>
+/// <param name="Title">Title of experience</param>
+/// <param name="Company">Name of company</param>
+/// <param name="Mode">Full remote, hybrid, on-site</param>
+/// <param name="Interval">Dates of start and end</param>
+/// <param name="Location">Location (if hybrid or on-site)</param>
+/// <param name="Description">Description of job</param>
+/// <param name="Responsibilities">Key responsibilities</param>
+/// <param name="Skills">Skills</param>
+public record Experience
+{
+	public string? Title { get; init; }
+	public string? Company { get; init; }
+	public string? Mode { get; init; }
+	public DateTime? StartDate { get; init; }
+	public DateTime? EndDate { get; init; }
+	public string? Location { get; init; }
+	public IEnumerable<string?>? Description { get; init; }
+	public IEnumerable<string?>? Responsibilities { get; init; }
+	public IEnumerable<string?>? Skills { get; init; }
+}
+public record Experiences
+{
+	public IEnumerable<Experience>? Items { get; init; }
+}
